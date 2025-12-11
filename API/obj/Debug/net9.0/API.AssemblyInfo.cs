b3e354d4840604dcb33fe3dfdc97295584e86b16
csabaialbert <68478226+csabaialbert@users.noreply.github.com>
2025-12-11 18:44:19 +0100

    Cloud DevOps basics mod

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6de7fdbba947edc8fa36ac86e28bbe9d4f26b131")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a3594670aef18b27641e59d25529902c47cca24")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
