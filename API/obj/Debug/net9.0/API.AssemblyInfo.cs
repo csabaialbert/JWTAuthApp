3ebe2f2843669f7c3ee803a6bd87133d8fe7a78e
csabaialbert <68478226+csabaialbert@users.noreply.github.com>
2025-12-13 19:12:32 +0100

    API bugfix #4

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c18233d7f5d99cb4181054b345ef77a5695045d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a747f8c1dde419edc09fd2483ba8855849fecdb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
